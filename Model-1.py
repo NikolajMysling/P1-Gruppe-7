a6719ccd598e6f09a1566806937e68dc456f01c9
Ziabo1 <sflaaris@gmail.com>
2025-10-19 22:57:48 +0200

    lille ændring til at starte op

---
+++ Model-1.py
@@ -0,0 +1,13 @@
+import pandas as pd                 
+import numpy as np                   
+import matplotlib.pyplot as plt      
+
+
+student_data = pd.read_csv('student_lifestyle_dataset.csv')
+
+print(student_data.info())
+
+print(student_data.head())
+
+
+
